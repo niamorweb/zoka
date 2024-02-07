8ceb9eb64b767d9d4e8160e01beae464259b70ba
romain <niamor.lth@gmail.com>
2024-02-07 22:06:35 +0100

    update

--- src/pages/api/logEnvVariable.ts
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  // Récupérez la valeur de votre variable d'environnement
-  const envVariableValue = process.env.SUPABASE_ANON_KEY;
-
-  // Enregistrez la valeur dans vos journaux côté serveur
-
-  // Répondez à la requête avec un message approprié
-  res.status(200).json({ message: envVariableValue });
-}
