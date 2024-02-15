cd5361dc1087bd19a6958eb050cf8bcc0dda4b99
romain <niamor.lth@gmail.com>
2024-02-15 15:07:15 +0100

    update

--- src/components/dashboard/appearance/appearance-form.tsx
+++ src/components/dashboard/appearance/appearance-form.tsx
@@ -17,6 +17,7 @@ import { supabase } from "@/lib/supabase";
 import { Button } from "@/components/ui/button";
 import { DataContext } from "@/utlis/userContext";
 import { toast } from "@/components/ui/use-toast";
+import Image from "next/image";
 
 const appearanceFormSchema = z.object({
   theme: z.enum(["light", "dark"], {
@@ -84,22 +85,15 @@ export function AppearanceForm() {
                       <RadioGroupItem value="light" className="sr-only" />
                     </FormControl>
                     <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
-                      <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                        <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-[#ecedef]" />
-                          <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]" />
-                        </div>
-                      </div>
+                      <Image
+                        src="/images/dashboard/light_demo.png"
+                        width={400}
+                        height={400}
+                        alt="dark appearance"
+                        className="w-full object-cover"
+                      />
                     </div>
-                    <span className="block w-full p-2 text-center font-normal">
+                    <span className="block w-full p-2 text-center font-medium">
                       Light
                     </span>
                   </FormLabel>
@@ -109,23 +103,16 @@ export function AppearanceForm() {
                     <FormControl>
                       <RadioGroupItem value="dark" className="sr-only" />
                     </FormControl>
-                    <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                      <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                        <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-2 w-[80px] rounded-lg bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                        <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                          <div className="h-4 w-4 rounded-full bg-slate-400" />
-                          <div className="h-2 w-[100px] rounded-lg bg-slate-400" />
-                        </div>
-                      </div>
+                    <div className="items-center rounded-md border-2 border-muted p-1 hover:border-accent">
+                      <Image
+                        src="/images/dashboard/dark_demo.png"
+                        width={400}
+                        height={400}
+                        alt="dark appearance"
+                        className="w-full object-cover"
+                      />
                     </div>
-                    <span className="block w-full p-2 text-center font-normal">
+                    <span className="block w-full p-2 text-center font-medium">
                       Dark
                     </span>
                   </FormLabel>
