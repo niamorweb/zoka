3d80a75c2aa8deb6789e53ce8165864df7b7caca
romain <niamor.lth@gmail.com>
2024-02-15 13:54:33 +0100

    update

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -42,32 +42,14 @@ export default function AuthenticationPage() {
           Sign up
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            Zoka
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;Sharing memories through images has never been easier.
-                This tool has revolutionized how I connect with friends and
-                family, making every moment unforgettable.&rdquo;
-              </p>
-              <footer className="text-sm">Emma Smith</footer>
-            </blockquote>
-          </div>
+          <Image
+            className="absolute left-0 object-cover top-0 right-0 bottom-0"
+            src="/images/auth/illustration.jpg"
+            width={1024}
+            height={1024}
+            alt=""
+          />
+          <span className="z-20 text-xl font-medium">Zoka</span>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
