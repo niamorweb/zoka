212c0b038e27d45d723029e2952e298aca15c5e5
romain <niamor.lth@gmail.com>
2024-02-18 20:07:27 +0100

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,12 +40,12 @@ export default function Home() {
         <Header />
         <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
           <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-            <Link
+            {/* <Link
               href="/"
               className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
             >
               Follow along on Twitter
-            </Link>
+            </Link> */}
             <h1 className="font-heading font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
               The platform for sharing your photos and links.
             </h1>
