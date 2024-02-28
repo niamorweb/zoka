ba0bc191aa9359a29b5ae135adb5792cb9c3c6c9
romain <niamor.lth@gmail.com>
2024-02-28 19:06:10 +0100

    update

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { Work_Sans } from "next/font/google";
 import Head from "next/head";
 import Link from "next/link";
+import { motion, AnimatePresence, useInView } from "framer-motion";
+import { useRef } from "react";
 
 interface link {
   name: String;
@@ -33,6 +35,43 @@ const fetchPhotos = async (userId: string) => {
 
   return dataItems;
 };
+
+function AnimTranslate({ children }: any) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section ref={ref}>
+      <div
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
+
+function AnimOpacity({ children }: any) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <section ref={ref}>
+      <div
+        style={{
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+        }}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
 
 const Gallery = ({ userInfos, photos }: any) => {
   if (userInfos) {
@@ -90,87 +129,97 @@ const Gallery = ({ userInfos, photos }: any) => {
                   )}
 
                   <div className="flex flex-col gap-3">
-                    {userInfos.avatar ? (
-                      <Image
-                        className={`w-24 lg:w-44 duration-150 h-24 lg:h-44 mb-4 object-cover rounded-full border-greenDark border-2`}
-                        src={`https://izcvdmliijbnyeskngqj.supabase.co/storage/v1/object/public/users_photos/${userInfos.id}/avatar/${userInfos.avatar}`}
-                        width={200}
-                        height={200}
-                        alt=""
-                      />
-                    ) : (
-                      <Image
-                        className={`w-24 lg:w-44 cursor-pointer  duration-150  h-24 lg:h-44 mb-4 object-cover rounded-full border-greenDark border-2`}
-                        src="/logo-large.jpg"
-                        width={200}
-                        height={200}
-                        alt=""
-                      />
-                    )}
-                    <h1 className="max-w-[600px] text-lg md:text-2xl lg:text-6xl font-extrabold tracking-widest">
-                      {userInfos.full_name}
-                    </h1>
-                    <p className={`max-w-[500px] mb-4 `}>{userInfos.bio}</p>
-                    {userInfos.links && (
-                      <div className="flex flex-col items-start md:flex-row md:flex-wrap gap-4 md:items-centers mt-4">
-                        {userInfos.links &&
-                          userInfos.links.map((link: any, index: any) => (
-                            <a
-                              key={index}
-                              href="https://www.youtube.com"
-                              target="_blank"
-                              className="inline-flex uppercase font-medium h-10 items-center rounded-full bg-black bg-opacity-20 px-4 backdrop-blur-md transition duration-700 ease-in-out hover:bg-white hover:text-black hover:duration-300"
-                            >
-                              <Image
-                                className="mr-2"
-                                height={16}
-                                width={16}
-                                src={`http://www.google.com/s2/favicons?domain=${link.url}`}
-                                alt=""
-                              />
+                    <AnimOpacity>
+                      {userInfos.avatar ? (
+                        <Image
+                          className={`w-24 lg:w-44 duration-150 h-24 lg:h-44 mb-4 object-cover rounded-full border-greenDark border-2`}
+                          src={`https://izcvdmliijbnyeskngqj.supabase.co/storage/v1/object/public/users_photos/${userInfos.id}/avatar/${userInfos.avatar}`}
+                          width={200}
+                          height={200}
+                          alt=""
+                        />
+                      ) : (
+                        <Image
+                          className={`w-24 lg:w-44 cursor-pointer  duration-150  h-24 lg:h-44 mb-4 object-cover rounded-full border-greenDark border-2`}
+                          src="/logo-large.jpg"
+                          width={200}
+                          height={200}
+                          alt=""
+                        />
+                      )}
+                    </AnimOpacity>
+                    <AnimOpacity>
+                      <h1 className="max-w-[600px] text-lg md:text-2xl lg:text-6xl font-extrabold tracking-widest">
+                        {userInfos.full_name}
+                      </h1>
+                    </AnimOpacity>
+                    <AnimOpacity>
+                      <p className={`max-w-[500px] mb-4 `}>{userInfos.bio}</p>
+                    </AnimOpacity>
+                    <AnimOpacity>
+                      {userInfos.links && (
+                        <div className="flex flex-col items-start md:flex-row md:flex-wrap gap-4 md:items-centers mt-4">
+                          {userInfos.links &&
+                            userInfos.links.map((link: any, index: any) => (
+                              <a
+                                key={index}
+                                href="https://www.youtube.com"
+                                target="_blank"
+                                className="inline-flex uppercase font-medium h-10 items-center rounded-full bg-black bg-opacity-20 px-4 backdrop-blur-md transition duration-700 ease-in-out hover:bg-white hover:text-black hover:duration-300"
+                              >
+                                <Image
+                                  className="mr-2"
+                                  height={16}
+                                  width={16}
+                                  src={`http://www.google.com/s2/favicons?domain=${link.url}`}
+                                  alt=""
+                                />
 
-                              <span className="">{link.name}</span>
-                              <ChevronRight className="w-5 h-5" />
-                            </a>
-                          ))}
-                      </div>
-                    )}
+                                <span className="">{link.name}</span>
+                                <ChevronRight className="w-5 h-5" />
+                              </a>
+                            ))}
+                        </div>
+                      )}
+                    </AnimOpacity>
                   </div>
                 </div>
                 <div className="columns-1 p-2 gap-2 sm:columns-2 xl:columns-3 2xl:columns-4">
                   {photos &&
                     photos.map((photo: any, index: number) => (
-                      <div
-                        key={index}
-                        className="after:content group relative mb-2 block w-full after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-                      >
-                        <Image
-                          placeholder="blur"
-                          blurDataURL={`/_next/image?url=https://izcvdmliijbnyeskngqj.supabase.co/storage/v1/object/public/users_photos/${userInfos.id}/gallery/${photo.image_url}?width=100&height=100/&w=16&q=1`}
-                          alt="Next.js Conf photo"
-                          className="transform transition will-change-auto"
-                          style={{ transform: "translate3d(0, 0, 0)" }}
-                          src={
-                            "https://izcvdmliijbnyeskngqj.supabase.co/storage/v1/object/public/users_photos/" +
-                            userInfos.id +
-                            "/gallery/" +
-                            photo.image_url +
-                            "?width=500&height=600"
-                          }
-                          width={720}
-                          height={480}
-                          quality={50}
-                          sizes="(max-width: 640px) 100vw,
+                      <AnimTranslate>
+                        <div
+                          key={index}
+                          className="after:content group relative mb-2 block w-full after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+                        >
+                          <Image
+                            placeholder="blur"
+                            blurDataURL={`/_next/image?url=https://izcvdmliijbnyeskngqj.supabase.co/storage/v1/object/public/users_photos/${userInfos.id}/gallery/${photo.image_url}?width=100&height=100/&w=16&q=1`}
+                            alt="Next.js Conf photo"
+                            className="transform transition will-change-auto"
+                            style={{ transform: "translate3d(0, 0, 0)" }}
+                            src={
+                              "https://izcvdmliijbnyeskngqj.supabase.co/storage/v1/object/public/users_photos/" +
+                              userInfos.id +
+                              "/gallery/" +
+                              photo.image_url +
+                              "?width=500&height=600"
+                            }
+                            width={720}
+                            height={480}
+                            quality={50}
+                            sizes="(max-width: 640px) 100vw,
                   (max-width: 1280px) 50vw,
                   (max-width: 1536px) 33vw,
                   25vw"
-                        />
-                        {photo.title !== "" && photo.title !== null && (
-                          <p className="absolute bottom-3 right-2 bg-white text-black font-semibold px-4 py-2 rounded-lg">
-                            {photo.title}
-                          </p>
-                        )}
-                      </div>
+                          />
+                          {photo.title !== "" && photo.title !== null && (
+                            <p className="absolute bottom-3 right-2 bg-white text-black font-semibold px-4 py-2 rounded-lg">
+                              {photo.title}
+                            </p>
+                          )}
+                        </div>
+                      </AnimTranslate>
                     ))}
                 </div>
               </main>
